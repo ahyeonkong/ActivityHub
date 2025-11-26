8a303ac67a784a59cd5f14c1ad130820bd96fe61
Suyun <justin2012@naver.com>
2025-11-26 17:55:24 +0900

    feat: comment insert 기능 구현

--- src/controller/CommentController.java
+++ src/controller/CommentController.java
@@ -2,12 +2,46 @@ package controller;
 
 import java.util.Scanner;
 
+import dto.CommentDTO;
+import service.CommentService;
+import view.CommentView;
+
 public class CommentController implements ControllerInterface {
 
 	@Override
 	public void execute(Scanner sc) {
-		// TODO Auto-generated method stub
-		
+
+		System.out.println("===== 댓글 메뉴 =====");
+		System.out.println("1. 댓글 작성");
+		System.out.println("0. 뒤로가기");
+		System.out.print("메뉴 선택 >> ");
+
+		int choice = Integer.parseInt(sc.nextLine());
+
+		switch (choice) {
+		case 1 -> insertComment(sc);
+		case 0 -> System.out.println("이전 메뉴로 돌아갑니다.");
+		default -> System.out.println("잘못된 입력입니다.");
+		}
 	}
 
+	private void insertComment(Scanner sc) {
+
+		System.out.print("Activity ID 입력: ");
+		int activityId = Integer.parseInt(sc.nextLine());
+
+		System.out.print("닉네임 입력: ");
+		String nickname = sc.nextLine();
+
+		System.out.print("댓글 내용 입력: ");
+		String content = sc.nextLine();
+
+		CommentDTO dto = new CommentDTO(0, activityId, nickname, content);
+
+		CommentService service = new CommentService();
+		CommentView view = new CommentView();
+
+		int result = service.createComment(dto);
+		view.printCreateResult(result);
+	}
 }
