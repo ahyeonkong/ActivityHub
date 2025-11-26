f9d05b886a61256c59e1f0a1ceb05a87ed0b7d0f
ahyeonkong <kimah0403@gmail.com>
2025-11-26 14:20:58 +0900

    chore: ControllerInterface 및 FrontController 초기 구조 추가[#1]
    프로젝트의 공통 ControllerInterface와 FrontController 초기 구조를 작성합니다.

--- src/common/ControllerInterface.java
+++ src/common/ControllerInterface.java
@@ -1,5 +1,8 @@
 package common;
 
+import java.util.Scanner;
+
 public interface ControllerInterface {
+	public abstract void execute(Scanner sc);
 
 }
--- src/common/FrontController.java
+++ src/common/FrontController.java
@@ -1,6 +1,48 @@
 package common;
 
+import java.util.Scanner;
+
+import controller.ActivityController;
+import controller.CommentController;
 
 public class FrontController {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ControllerInterface controllerInterface = null;
 
+		boolean isStop = false;
+		while (!isStop) {
+			System.out.println("====================MENU====================");
+			System.out.println();
+			System.out.println("0.종료 | 1.Activity | 2.Comments");
+			System.out.println();
+			System.out.println("============================================");
+			System.out.print("메뉴를 선택하세요. >> ");
+			try {
+				int job = Integer.parseInt(sc.nextLine());
+
+				switch (job) {
+				case 0 -> {
+					isStop = true;
+					controllerInterface = null;
+				}
+				case 1 -> {
+					controllerInterface = new ActivityController();
+				}
+				case 2 -> {
+					controllerInterface = new CommentController();
+				}
+				default -> {
+					controllerInterface = null;
+				}
+				}
+				if (controllerInterface != null)
+					controllerInterface.execute(sc);
+			} catch (NumberFormatException e) {
+				System.out.println("숫자만 입력해주세요.");
+			}
+		}
+		sc.close();
+
+	}
 }
